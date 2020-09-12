3af2810bbba2dc416cec8556cd634f529636373c
Vitaliy <theyamshikov@yandex.ru>
2020-09-12 18:11:59 +0600

    upd

--- bin/composer/git pull dev.sh
+++ bin/composer/git pull dev.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd ../../vendor
 git clone "git@github.com:php7lab/core.git" "php7lab-dev/core"
-git clone "git@github.com:php7lab/eloquent.git" "php7lab-dev/eloquent"
+git clone "git@github.com:zncore/db.git" "php7lab-dev/eloquent"
